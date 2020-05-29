8eeca6b705f169e0a0ad2220b13eb1ec4ed2d750
Matthew Hummer <biggreyson@gmail.com>
2020-05-29 11:11:46 -0600

    POC changes?

---
+++ AutoLogPOCClient/app/src/main/java/com/mattalui/autologpocclient/services/Config.java
@@ -0,0 +1,8 @@
+package com.mattalui.autologpocclient;
+
+public class Config {
+  public static final boolean VERBOSE = true;
+  public static final String HOST = "http://10.253.227.80:1337";
+  public static final String APPID = "can_we_keep_her";
+  public static final String USERTOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJtYXR0QGV4YW1wbGUuY29tIiwiaWF0IjoxNTY4MDYzNTQyfQ.F8qOfLSYVdwzSkIYiqTsD3tcw2WBo7ezc25tLFenSuY";
+}
